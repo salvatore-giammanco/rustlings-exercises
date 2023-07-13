0341aa6da1ceaf2e46a73ebaaf4513e141d438d1
Salvatore Giammanco <salvatore@giammanco.me>
2023-07-13 22:01:11 +0200

    I decided to version my Rustlings journey, kinda late

---
+++ clippy/target/debug/deps/clippy1-f43ec081b640b13a.d
@@ -0,0 +1,7 @@
+/Users/salvatoregiammanco/Workspace/playground/rustlings/exercises/clippy/target/debug/deps/clippy1-f43ec081b640b13a.rmeta: clippy1.rs
+
+/Users/salvatoregiammanco/Workspace/playground/rustlings/exercises/clippy/target/debug/deps/clippy1-f43ec081b640b13a.d: clippy1.rs
+
+clippy1.rs:
+
+# env-dep:CLIPPY_ARGS=-D__CLIPPY_HACKERY__warnings__CLIPPY_HACKERY__-D__CLIPPY_HACKERY__clippy::float_cmp__CLIPPY_HACKERY__
